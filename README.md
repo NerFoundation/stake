deb54d62059940e2e17aafb04da268156bda84b3
Sheraz Arshad <sheraz1392@gmail.com>
2018-09-17 09:48:43 +0300

    deployment steps

---
+++ README.md
@@ -0,0 +1,13 @@
+### Deployment Steps
+
+## Deployment to Ropsten Network
+
+```
+truffle migrate --network ropsten
+```
+
+## Deployment to Main Nerwork
+
+```
+truffle migrate --network mainnet
+```
--- truffle.js
+++ truffle.js
@@ -11,6 +11,9 @@
  *     gasPrice: 10000000000,
  *   },
  */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "avocado armed also position solution total token maze deny neutral bless share";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,6 +23,24 @@ module.exports = {
       host:"localhost",
       port:"8545",
       network_id:"*"
-    }
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/QWMgExFuGzhpu2jUr6Pq")
+      },
+      network_id: 1,
+      gas: 4712388,
+      gasPrice: 10000000000
+
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/QWMgExFuGzhpu2jUr6Pq",0,9)
+      },
+      network_id: 3,
+      gas: 4712388,
+      gasPrice: 10000000000
+
+    } 
   }
 };
